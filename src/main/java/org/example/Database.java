5732e033aecc16ae1e97ee8a4fe39fa210be5751
Nazar868 <zagariynazar04@gmail.com>
2026-06-19 14:44:24 +0300

    Create Database.java

---
+++ src/main/java/org/example/Database.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static Database instance;
+    private Connection connection;
+
+    private static final String URL =
+            "jdbc:h2:./h2db";
+
+    private Database() {
+
+        try {
+            connection = DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Database getInstance() {
+
+        if (instance == null) {
+            instance = new Database();
+        }
+
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
